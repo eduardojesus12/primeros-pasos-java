d0800208fdf9b7d7ff854372d208559db460de55
Carlos Eduardo <c.eduardo.jesus@outlook.com>
2022-01-27 15:43:37 -0600

    Primeros pasos en Java

---
+++ JavaBasicos/src/VariablesTiposDeDatos.java
@@ -0,0 +1,46 @@
+
+public class VariablesTiposDeDatos {
+	
+	public static void main(String[] args) {
+		
+		// String permite almacenar cadenas de texto
+		String nombre = "Carlos Eduardo";
+		//int edad = 31;
+		
+		// char nos permite almacenar un caracter, pero sólo 1
+		char inicial = 'C';
+		
+		// boolean almacena un valor booleano, puede ser true o false
+		boolean tienesCrush = true;
+		
+		// tipos de datos enteros
+		
+		//byte
+		byte edad = 31;
+		
+		// short
+		short suma = 24 + 104;
+		
+		//int
+		int numero = 1021321312;
+		//long
+		
+		long numeroLargo = 100_000_000;
+		
+		System.out.println(numeroLargo);
+		
+		// float
+		float promedio = 1232.23232f;
+		
+		// double
+		double numeroPi = 3.1415934324234;
+		
+		
+		/*
+		Javascript
+		let numero = 10;
+		let nombre = 'Carlos Eduardo'
+		*/
+	}
+
+}
